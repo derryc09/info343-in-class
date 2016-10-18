336dd604dc3a10b9c3467ca97a6660bbb0b06ed8
Derry Cheng <derryc09@uw.edu>
2016-10-18 15:21:03 -0700

    added animations

--- lab/dom/js/main.js
+++ lab/dom/js/main.js
@@ -6,3 +6,16 @@ var boxOne = document.getElementById('one'),
 	boxFour = document.getElementById('four'),
 	boxFive = document.getElementById('five'),
 	boxSix = document.getElementById('six');
+
+
+
+console.log("hello world!");
+
+boxOne.addEventListener("click", changeBackground);
+
+function changeBackground(){
+	boxOne.classList.add("fade-to-black");
+
+}
+
+console.log("JS runs");
