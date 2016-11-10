f683427bb1d4c31d73a09d962af04c9c3d419e41
Derry Cheng <derryc09@uw.edu>
2016-11-10 12:16:15 -0800

    adding files

--- redux/src/favorite-list.jsx
+++ redux/src/favorite-list.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 
 import UserCard from "./user-card.jsx";
+import {store, removeFavorites} from "./shared-state.js";
 
 export default class extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = store.getState();
     }
 
+    componentDidMount() {
+        this.unsub = store.subscribe(() => this.setState(store.getState()));
+    }
+    comonentWillUnmount(){
+        this.unsub();
+    }
     render() {
         var userCards;
         if (this.state.favorites) {
             userCards = this.state.favorites.map(record => 
                 <UserCard key={record.id} 
                     user={record}>
-                    <button>Remove</button>
+                    <button onClick={() => store.dispatch(removeFavorites(record.id))}>Remove</button>
                 </UserCard>);
         }
         return (
